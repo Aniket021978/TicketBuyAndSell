daf0d3bd8dc088fe4d9ef59aeae6c5bb244082c3
Aniket Bansal <96112371+Aniket021978@users.noreply.github.com>
2024-11-20 12:42:10 +0530

    Update GetTickets.js

--- front/src/components/GetTickets.js
+++ front/src/components/GetTickets.js
@@ -19,7 +19,7 @@ const GetTickets = () => {
   useEffect(() => {
     const fetchTickets = async () => {
       try {
-        const response = await fetch("http://localhost:5000/tickets");
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/tickets`);
         if (!response.ok) throw new Error("Failed to fetch tickets");
         const data = await response.json();
         setTickets(data);
