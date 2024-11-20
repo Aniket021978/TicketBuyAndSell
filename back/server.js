2846f9d00d1ef0ef6b93e55e7bd6b2195a50d807
Aniket Bansal <96112371+Aniket021978@users.noreply.github.com>
2024-11-20 12:29:54 +0530

    Update server.js

--- back/server.js
+++ back/server.js
@@ -10,13 +10,13 @@ const JWT_SECRET = "your_jwt_secret_key";
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-
+require('dotenv').config();
 app.use(
   cors()
 );
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/TickNix", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
